ab95ccc0a36a97eb240c9b2000a8de5bee243ba5
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-10-13 18:58:44 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="social-ethosa",
-    version="0.2.2",
+    version="0.3.3",
     author="Ethosa",
     author_email="social.ethosa@gmail.com",
     description="The social ethosa library for Python",
@@ -13,7 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Ethosa/social_ethosa",
     packages=setuptools.find_packages(),
+    license="MIT",
+    keywords="vk api botwrapper network math social ethosa",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
@@ -23,7 +26,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    project_urls={
+        "Github" : "https://github.com/Ethosa/social_ethosa",
+        "Documentation" : "https://github.com/Ethosa/social_ethosa/blob/master/README.md",
+    },
+    python_requires=">=2.7",
     install_requires=[
-        'requests'
+        "requests"
     ]
 )
