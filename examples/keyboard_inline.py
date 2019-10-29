3eeaefdbea853e639ccafa33ac46e23deb4e93af
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-10-29 22:51:26 +0700

    добавлены примеры

---
+++ examples/keyboard_inline.py
@@ -0,0 +1,37 @@
+# -*- coding; utf-8 -*-
+# author: Ethosa
+
+# we import the functionality we need
+from social_ethosa import Keyboard, Button, __version__ as seVersion, printf
+
+# Let's create a single button
+my_first_button = Button("text", label="Hello world :)")
+
+# Initialize the inline keyboard
+keyboard = Keyboard(inline=True)
+
+# Add this button to it several times
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+# Note that the addLine() method is optional
+# because the keyboard automatically jumps to a new line if necessary.
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+keyboard.addButton(my_first_button)
+
+# draw a keyboard in the console
+keyboard.visualize()
+
+# we get a compiled keyboard
+compiled = keyboard.compile()
+
+# Displaying the compiled version
+printf(compiled)
+
+# get the version of the library itself
+printf("\nversion %s", seVersion)
