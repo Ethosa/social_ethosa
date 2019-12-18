38fca63cede6eb19bcb20d5f3b78aa0ee08f8b8a
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-12-18 23:50:02 +0700

    markovChains - docstrings was added

--- social_ethosa/extra/markovChains.py
+++ social_ethosa/extra/markovChains.py
@@ -9,18 +9,41 @@ class MarkovChains:
         self.magic = lambda s: s.lstrip(">").rstrip("<").strip()
 
     def addChain(self, name, value):
+        """Adds a new chain.
+
+        Arguments:
+            name {str} -- chain name
+            value {str} -- other chain name
+        """
         if name not in self.chains:
             self.chains[name] = [value]
         else:
             self.chains[name].append(value)
 
     def deleteChain(self, name):
+        """Removes all links to this chain.
+
+        Arguments:
+            name {str} -- chain name
+        """
         for key in self.chains:
             while name in self.chains[key]:
                 self.chains[key].remove(name)
         del self.chains[name]
 
     def generateSequence(self, length, auth=None):
+        """generates a list, selects random keys from a dictionary
+
+        Arguments:
+            length {int} -- list length
+
+        Keyword Arguments:
+            auth {str} -- the key from which the generation begins.
+                If not cauldron, then randomly selected (default: {None})
+
+        Returns:
+            list -- generated list
+        """
         if not auth:
             auth = choice([key for key in self.chains])
         current = self.chains[auth]
@@ -32,6 +55,19 @@ class MarkovChains:
         return out
 
     def execute(self, string):
+        """Adds new chains based on the passed string
+
+        Arguments:
+            string {str} -- string to execute
+
+        Example using:
+            execute("z => y <=> x <= z")
+            this is equivalent to the following code:
+            addChain("z", "y")
+            addChain("y", "x")
+            addChain("x", "y")
+            addChain("z", "x")
+        """
         out = string.replace("=", "-").split("-")
         for i in enumerate(out):
             current = out[i]
